9828a5872b2a9751a280a5b94eef985336f0997a
johnelias1 <donniche15@gmail.com>
2022-09-05 20:07:00 -0500

    navbar e imagen de home

--- src/shared/sharedComponents.module.ts
+++ src/shared/sharedComponents.module.ts
@@ -9,6 +9,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+
 @NgModule({
   declarations: [],
   imports: [
@@ -20,6 +22,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
+    MatFormFieldModule,
+    MatCardModule
   ],
   exports: [
     MatSliderModule,
@@ -29,6 +33,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
+    MatFormFieldModule,
+    MatCardModule
+
   ]
 })
 export class SharedModule { }
